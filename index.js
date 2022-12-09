c0d672fe223888e902ebf381c591d5e2f3485809
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-09 10:41:20 -0300

    Adding serving client side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 
 const companyRoutes = require('./routes/companyRoutes');
@@ -33,6 +34,11 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //routes
 app.use('/api/company', companyRoutes);

b346fc26aaa610450f733e95e7d3bbf1273984e4
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 19:07:18 -0300

    adding methods

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use(cors());
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
 
   next();
 });

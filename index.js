94f83646155892b744d4af399276e71003f98e14
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 16:53:41 -0300

    cors 2

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ app.listen(process.env.PORT || 5000, () => {
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://sigus-app.vercel.app/login"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //routes
 app.use('/api/company', companyRoutes);
 app.use('/api', authRoutes);

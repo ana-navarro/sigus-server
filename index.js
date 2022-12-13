6d35fd24cca63d22edcb5c3781c94df5b1631bc8
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 17:25:57 -0300

    All

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.use(cors())
 app.use(cors({ origin: true, credentials: true }));
-// app.options("*", cors({ origin: 'http://181.215.134.184:5000', optionsSuccessStatus: 200 }));
+app.options("*", cors({ origin: 'http://181.215.134.184:5000', optionsSuccessStatus: 200 }));
 
-// app.use(cors({ origin: "http://181.215.134.184:5000", optionsSuccessStatus: 200 }));
+app.use(cors({ origin: "http://181.215.134.184:5000", optionsSuccessStatus: 200 }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://sigus-app.vercel.app");

81765116f23906036e8d39272d1ce687a62aea1a
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 16:55:15 -0300

    change cors url

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(process.env.PORT || 5000, () => {
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://sigus-app.vercel.app/login");
+  res.header("Access-Control-Allow-Origin", "https://sigus-app.vercel.app/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });

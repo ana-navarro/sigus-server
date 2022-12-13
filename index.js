d843094295e32cb893105f934d9c77ed1cbef43c
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 18:03:06 -0300

    adding acao abd acah

--- index.js
+++ index.js
@@ -32,17 +32,18 @@ app.listen(process.env.PORT || 5000, () => {
   connect();
 });
 
-app.use(cors())
+app.use(cors());
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  next();
+});
+
 // app.use(cors({ origin: true, credentials: true }));
 // app.options("*", cors({ origin: 'http://181.215.134.184:5000', optionsSuccessStatus: 200 }));
 
 // app.use(cors({ origin: "http://181.215.134.184:5000", optionsSuccessStatus: 200 }));
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://sigus-app.vercel.app");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 app.use(express.json());
 

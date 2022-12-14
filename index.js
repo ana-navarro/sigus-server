fb882e07c7c4a3c153f8e14b5c6c25ff2706b941
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 22:55:43 -0300

    taking out credentials

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', 'false');
 
   next();
 });

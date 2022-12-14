073fa5a42afb22a3147feadd0099e9428b203c12
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 22:54:16 -0300

    adding credentials

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   next();
 });
